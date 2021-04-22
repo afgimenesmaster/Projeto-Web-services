5d474d046556765233bb5d9f364d0e6f4f5d9071
Anderson Fagundes Gimenes <anderson.fagundes.gimenes@hotmail.com>
2021-04-21 22:40:22 -0300

    OrderStatus enum

--- src/main/java/com/projeto/negocio/config/TestConfig.java
+++ src/main/java/com/projeto/negocio/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.projeto.negocio.entities.Order;
 import com.projeto.negocio.entities.User;
+import com.projeto.negocio.entities.enums.OrderStatus;
 import com.projeto.negocio.repositories.OrderRepository;
 import com.projeto.negocio.repositories.UserRepository;
 
@@ -22,16 +23,18 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private OrderRepository orderRepository;
-
+	
+	
 	@Override
 	public void run(String... args) throws Exception {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
 		
-		Order o1 = new Order(null, Instant.parse("2021-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2021-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2021-07-22T15:21:22Z"), u1);
+		
+		Order o1 = new Order(null, Instant.parse("2021-07-21T03:42:10Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2021-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2021-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,u1);
 		
 		
 		
