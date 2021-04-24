99505731c8ea73a2081bc8d1737277f40d41bb6f
Anderson Fagundes Gimenes <anderson.fagundes.gimenes@hotmail.com>
2021-04-24 18:51:40 -0300

    Subtotal & Total methods

--- src/main/java/com/projeto/negocio/entities/OrderItem.java
+++ src/main/java/com/projeto/negocio/entities/OrderItem.java
@@ -74,6 +74,13 @@ public class OrderItem implements Serializable{
 	}
 				
 
+	public Double getSubTotal() {
+		return price * quantity;
+	}
+	
+	
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
