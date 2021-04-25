8aa7d80a2cf5013bff842035995e906aba6a1a78
Anderson Fagundes Gimenes <anderson.fagundes.gimenes@hotmail.com>
2021-04-24 21:30:14 -0300

    Exception handling - delete

--- src/main/java/com/projeto/negocio/services/UserService.java
+++ src/main/java/com/projeto/negocio/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.projeto.negocio.entities.User;
 import com.projeto.negocio.repositories.UserRepository;
+import com.projeto.negocio.services.exceptions.DatabaseException;
 import com.projeto.negocio.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -32,7 +35,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		};
 	}
 	
 	
